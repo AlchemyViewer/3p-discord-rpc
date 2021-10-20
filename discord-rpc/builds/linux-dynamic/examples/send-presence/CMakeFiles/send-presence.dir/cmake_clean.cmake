1b54e84b706977e3185a5fa8090db00d8893bc87
Xenhat Hex <me@xenh.at>
2021-10-19 21:37:06 -0400

    add discord_rpc source

---
+++ discord-rpc/builds/linux-dynamic/examples/send-presence/CMakeFiles/send-presence.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/send-presence.dir/send-presence.c.o"
+  "CMakeFiles/send-presence.dir/send-presence.c.o.d"
+  "send-presence"
+  "send-presence.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/send-presence.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ discord-rpc/builds/linux-dynamic/src/CMakeFiles/discord-rpc.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/xenhat/code/vwr/libs/discord-rpc/discord-rpc/src/connection_unix.cpp" "src/CMakeFiles/discord-rpc.dir/connection_unix.cpp.o" "gcc" "src/CMakeFiles/discord-rpc.dir/connection_unix.cpp.o.d"
+  "/home/xenhat/code/vwr/libs/discord-rpc/discord-rpc/src/discord_register_linux.cpp" "src/CMakeFiles/discord-rpc.dir/discord_register_linux.cpp.o" "gcc" "src/CMakeFiles/discord-rpc.dir/discord_register_linux.cpp.o.d"
+  "/home/xenhat/code/vwr/libs/discord-rpc/discord-rpc/src/discord_rpc.cpp" "src/CMakeFiles/discord-rpc.dir/discord_rpc.cpp.o" "gcc" "src/CMakeFiles/discord-rpc.dir/discord_rpc.cpp.o.d"
+  "/home/xenhat/code/vwr/libs/discord-rpc/discord-rpc/src/rpc_connection.cpp" "src/CMakeFiles/discord-rpc.dir/rpc_connection.cpp.o" "gcc" "src/CMakeFiles/discord-rpc.dir/rpc_connection.cpp.o.d"
+  "/home/xenhat/code/vwr/libs/discord-rpc/discord-rpc/src/serialization.cpp" "src/CMakeFiles/discord-rpc.dir/serialization.cpp.o" "gcc" "src/CMakeFiles/discord-rpc.dir/serialization.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
